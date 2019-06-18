c2b8d0aca139cbc12628efb0e83d1c56523e0d81
Steven Kurniawan <stevenkurkur@gmail.com>
2019-06-18 16:32:01 +0900

    first commit, add dataset

---
+++ dataset.py
@@ -0,0 +1,56 @@
+from azure.cosmosdb.table.tableservice import TableService
+from azure.cosmosdb.table.models import Entity
+
+the_connection_string = "DefaultEndpointsProtocol=https;AccountName=bigdatasystem;AccountKey=dlGS2h7vRiEdwRjGw1R2UqCr3QcJUGzfQjeyULj12Ssf1NtKwA11upx3uu1x1KLTBUzX00waXTelLQfY6oQ9BA==;TableEndpoint=https://bigdatasystem.table.cosmos.azure.com:443/;"
+table_service = TableService(endpoint_suffix = "table.cosmos.azure.com", connection_string = the_connection_string)
+
+userid = []
+date = []
+hour = []
+minute = []
+second = []
+longitude = []
+latitude = []
+i = 0
+
+for x in range(1,2): #10358 ~ data dari 1-10357
+	with open("dataset/taxi_log_2008_by_id/{}.txt".format(str(x))) as f:
+		for line in f:
+			data = line.split(",")
+			#userid.append(data[0])
+			userid = data[0]
+
+			datetime = data[1]
+			partitionkey = datetime.replace("-","").replace(" ","").replace(":","")
+
+			split = datetime.split(" ")
+			#date.append(split[0])
+			date = split[0]
+
+			times = split[1].split(":")
+			#hour.append(times[0])
+			hour = times[0]
+			#minute.append(times[1])
+			minute = times[1]
+			#second.append(times[2])
+			second = times[2]
+
+			#longitude.append(data[2])
+			longitude = data[2]
+			#latitude.append(data[3])
+			latitude = data[3]
+
+			entity = Entity()
+			entity.PartitionKey = partitionkey
+			entity.RowKey = str(i)
+			entity.userid = userid
+			entity.t_date = date
+			entity.t_hour = hour
+			entity.t_minute = minute
+			entity.t_second = second
+			entity.longitude = longitude
+			entity.latitude = latitude
+			table_service.insert_entity('dataset', entity)
+
+			i += 1
+
